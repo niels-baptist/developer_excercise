e5791053a841c24b113be97af6c63d68ef3dca23
Niels baptist <r0793266@student.thomasmore.be>
2021-12-28 20:29:08 +0100

    working

--- app.js
+++ app.js
@@ -10,10 +10,10 @@
 connect();
 
 async function connect() {
-    const urlQlikServer = "https://atonce-msp.eu.qlikcloud.com";
+    const urlQlikServer = "https://ewdg25ot2lurgg6.eu.qlikcloud.com";
     const urlLoggedIn = "/api/v1/audits";//Use GET request to see if you are authenticated
     const urlLogin = "/login";
-    const webIntegrationId = 'Dz94dfu7Xu80gGd2Lgl8hf8BewV7ZXcl';        
+    const webIntegrationId = 'oqmuZuCVsyZlV9FL2wcEMtA79uvVXYRO';        
 
     //Check to see if logged in
     return await fetch(`${urlQlikServer}${urlLoggedIn}`, {
@@ -27,7 +27,7 @@ async function connect() {
         //check if user is authenticated; if not, redirect to login page
 		if(response.status===401){
             const url = new URL(`${urlQlikServer}/login`);
-            url.searchParams.append('returnto', 'http://localhost/ThomasMoreTutorial/TM_Tutorial.html');
+            url.searchParams.append('returnto', 'http://localhost/developer_excercise/index.html');
             url.searchParams.append('qlik-web-integration-id', webIntegrationId);
             window.location.href = url;
         }	
@@ -39,11 +39,11 @@ async function connect() {
 }		
  
 var config1 = {
-    host: "atonce-msp.eu.qlikcloud.com", //the address of your Qlik Engine Instance
+    host: "ewdg25ot2lurgg6.eu.qlikcloud.com", //the address of your Qlik Engine Instance
     prefix: "/", //or the virtual proxy to be used. for example "/anonymous/"
     port: 443, //or the port to be used if different from the default port  
     isSecure: true, //should be true if connecting over HTTPS
-    webIntegrationId: 'Dz94dfu7Xu80gGd2Lgl8hf8BewV7ZXcl' //only needed in SaaS editions and QSEoK
+    webIntegrationId: 'oqmuZuCVsyZlV9FL2wcEMtA79uvVXYRO' //only needed in SaaS editions and QSEoK
 };
 
 require.config( {
@@ -62,7 +62,7 @@ require( ["js/qlik"], function ( qlik ) {
 	} );
 	
 	//open apps -- inserted here --
-	var app = qlik.openApp("8a167694-09fb-48fe-ac75-d670ffe70685", config1);
+	var app = qlik.openApp("3efcebb1-197e-43b6-8baf-f767cc21aedb", config1);
 	
 	//get objects -- inserted here --
 	app.getObject('QV01', 'NyNEsWz');
