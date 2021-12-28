ed7ef52d52a5c24aad7c4195a3094e025e7d69d1
Niels baptist <r0793266@student.thomasmore.be>
2021-12-28 16:19:17 +0100

    used tutorial from canvas

--- README.md
+++
@@ -1,4 +0,0 @@
-# developer_excercise
-
-Here you can find my take on the developer excercise for the BI project, in which we embed a Qlik Sense app into a modern website using the integration tools from Qlik Sense.
-My site is hosted at https://biniels.sinners.be.
---
+++ TM_Tutorial.js
@@ -0,0 +1,83 @@
+/*
+ * Basic responsive mashup template
+ * @owner Enter you name here (xxx)
+ */
+/*
+ *    Fill in host and port for Qlik engine
+ */
+ 
+ 
+connect();
+
+async function connect() {
+    const urlQlikServer = "https://atonce-msp.eu.qlikcloud.com";
+    const urlLoggedIn = "/api/v1/audits";//Use GET request to see if you are authenticated
+    const urlLogin = "/login";
+    const webIntegrationId = 'Dz94dfu7Xu80gGd2Lgl8hf8BewV7ZXcl';        
+
+    //Check to see if logged in
+    return await fetch(`${urlQlikServer}${urlLoggedIn}`, {
+        credentials: 'include',
+        headers: {                  
+            'Qlik-Web-Integration-ID':webIntegrationId
+        }
+    })
+    .then(async function(response)
+    {
+        //check if user is authenticated; if not, redirect to login page
+		if(response.status===401){
+            const url = new URL(`${urlQlikServer}/login`);
+            url.searchParams.append('returnto', 'http://localhost/ThomasMoreTutorial/TM_Tutorial.html');
+            url.searchParams.append('qlik-web-integration-id', webIntegrationId);
+            window.location.href = url;
+        }	
+    })
+    .catch(function(error)
+    {
+        console.error(error);
+    });	
+}		
+ 
+var config1 = {
+    host: "atonce-msp.eu.qlikcloud.com", //the address of your Qlik Engine Instance
+    prefix: "/", //or the virtual proxy to be used. for example "/anonymous/"
+    port: 443, //or the port to be used if different from the default port  
+    isSecure: true, //should be true if connecting over HTTPS
+    webIntegrationId: 'Dz94dfu7Xu80gGd2Lgl8hf8BewV7ZXcl' //only needed in SaaS editions and QSEoK
+};
+
+require.config( {
+    baseUrl: (config1.isSecure ? "https://" : "http://" ) + config1.host + (config1.port ? ":" + config1.port : "") + config1.prefix + "resources",
+    webIntegrationId: config1.webIntegrationId
+} );	
+
+require( ["js/qlik"], function ( qlik ) {
+	
+	qlik.on( "error", function ( error ) {
+		$( '#popupText' ).append( error.message + "<br>" );
+		$( '#popup' ).fadeIn( 1000 );
+	} );
+	$( "#closePopup" ).click( function () {
+		$( '#popup' ).hide();
+	} );
+	
+	//open apps -- inserted here --
+	var app = qlik.openApp("8a167694-09fb-48fe-ac75-d670ffe70685", config1);
+	
+	//get objects -- inserted here --
+	app.getObject('QV01', 'NyNEsWz');
+	
+	var Imgsettings = {format: 'png', height: 300, width: 800 };
+	var settings = { documentSize: 'a4', aspectRatio: 2, orientation: "landscape" }
+	
+        app.visualization.get('NyNEsWz').then(function(vis){
+		vis.exportImg(Imgsettings).then(function (result) {
+		console.log('PDF download link: ', result);
+		});
+		});
+		
+	$("#ClearAll").click(function() {
+	app.clearAll();
+      });
+
+} );
